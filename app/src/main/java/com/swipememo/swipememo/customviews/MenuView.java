5c71d0e9da67d704b7514f27b199b994da124dd7
DoDo <kdh7337@gmail.com>
2017-04-12 22:07:35 +0900

    메뉴버튼 항시 보이기

--- app/src/main/java/com/swipememo/swipememo/customviews/MenuView.java
+++ app/src/main/java/com/swipememo/swipememo/customviews/MenuView.java
@@ -39,32 +39,33 @@ public class MenuView extends android.support.v7.widget.AppCompatButton {
 
     @Override
     public void setVisibility(int visibility) {
-        long now = Calendar.getInstance().getTimeInMillis();
-        switch (visibility) {
-            case VISIBLE:
-                Log.e("MenuView", "VISIBLE ANIM START");
-                if (getVisibility() == GONE)
-                    super.setVisibility(INVISIBLE);
-                setScaleX(0.0f);
-                setScaleY(0.0f);
-                super.setVisibility(visibility);
-                animate().scaleX(1.0f).setDuration(ANIM_DURATION).start();
-                animate().scaleY(1.0f).setDuration(ANIM_DURATION).start();
-                break;
-
-            case INVISIBLE:
-                Log.e("MenuView", "INVISIBLE ANIM START");
-                animate().scaleX(0.0f).setDuration(ANIM_DURATION).start();
-                animate().scaleY(0.0f).setDuration(ANIM_DURATION).start();
-                mVHandler.sendEmptyMessageAtTime(INVISIBLE, now + ANIM_DURATION);
-                break;
-            case GONE:
-                Log.e("MenuView", "GONE ANIM START");
-                animate().scaleX(0.0f).setDuration(ANIM_DURATION).start();
-                animate().scaleY(0.0f).setDuration(ANIM_DURATION).start();
-                mVHandler.sendEmptyMessageAtTime(GONE,now + ANIM_DURATION);
-                break;
-        }
+//        long now = Calendar.getInstance().getTimeInMillis();
+//        switch (visibility) {
+//            case VISIBLE:
+//                Log.e("MenuView", "VISIBLE ANIM START");
+//                if (getVisibility() == GONE)
+//                    super.setVisibility(INVISIBLE);
+//                setScaleX(0.0f);
+//                setScaleY(0.0f);
+//                super.setVisibility(visibility);
+//                animate().scaleX(1.0f).setDuration(ANIM_DURATION).start();
+//                animate().scaleY(1.0f).setDuration(ANIM_DURATION).start();
+//                break;
+//
+//            case INVISIBLE:
+//                Log.e("MenuView", "INVISIBLE ANIM START");
+//                animate().scaleX(0.0f).setDuration(ANIM_DURATION).start();
+//                animate().scaleY(0.0f).setDuration(ANIM_DURATION).start();
+//                mVHandler.sendEmptyMessageAtTime(INVISIBLE, now + ANIM_DURATION);
+//                break;
+//            case GONE:
+//                Log.e("MenuView", "GONE ANIM START");
+//                animate().scaleX(0.0f).setDuration(ANIM_DURATION).start();
+//                animate().scaleY(0.0f).setDuration(ANIM_DURATION).start();
+//                mVHandler.sendEmptyMessageAtTime(GONE,now + ANIM_DURATION);
+//                break;
+//        }
+        super.setVisibility(visibility);
     }
     class VisibilityHandler extends Handler{
         @Override
